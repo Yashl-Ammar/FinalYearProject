fe23edd3c46db91408a0e787eae18c0b2a8acdee
Yashl-Ammar <yashlammarr@gmail.com>
2023-10-19 23:11:07 +0500

    first commit

---
+++ jobmanagement/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+
+const JobRouter = require("./Routes/Job.Route");
+
+const app = express();
+app.use(cors());
+
+const DB_URI = process.env.DB_URI;
+const port = process.env.port || 3090;
+
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB"));
+
+app.listen(port, () => {
+  console.log(`The Server is running on Port ${port}`);
+});
+
+app.get('/', (req,res) => {
+  res.json({'message':'Hello There!'})
+})
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/jobmanagement", JobRouter);
