beb0cac91ff509c435ad38b66e70f604d17c5e83
SHEIKHSAAD2561 <saadamin2561@gmail.com>
2023-12-12 17:10:05 -0800

    Report Management Module Added

---
+++ ReportManagement/Controller/ReportController.js
@@ -0,0 +1,80 @@
+const Report=require('../Models/Report')
+
+const addReportByClient=async(req,res)=>{
+    const{title,description,details,links,type}=req.body
+
+    const report=new Report({
+        client:req.user._id,
+        title,
+        description,
+        details,
+        links,
+        type
+    })
+
+    const newReport=await report.save()
+
+    res.send(newReport)
+}
+const addReportByfreelancer=async(req,res)=>{
+    const{title,description,details,links,type}=req.body
+
+    const report=new Report({
+        freelancer:req.user._id,
+        title,
+        description,
+        details,
+        links,
+        type
+    })
+
+    const newReport=await report.save()
+
+    res.send(newReport)
+}
+const viewReports=async(req,res)=>{
+    const reports=await Report.find({})
+    if(!reports) return res.status(404).send("Reports Not Found")
+    res.send(reports)
+}
+const viewSpecificReport=async(req,res)=>{
+    const id =req.params.reportId
+    const report=await Report.findById(id)
+    if(!report) return res.status(404).send("Report Not Found")
+    res.send(report)
+}
+const resolveReport=async(req,res)=>{
+    const id =req.params.reportId
+    const report=await Report.findById(id)
+    if(!report) return res.status(404).send("Report Not Found")
+    report.status="Resolved"
+    const newReport=await report.save()
+    res.send(newReport)
+}
+const deleteReport=async(req,res)=>{
+    const id =req.params.reportId
+    const report=await Report.findByIdAndDelete(id)
+    if(!report) return res.status(404).send("Report Not Found")
+    res.send("Report Deleted Successfully")
+}
+const updateReport=async(req,res)=>{
+    const id =req.params.reportId
+    const{title,description,details}=req.body
+    const report=await Report.findById(id)
+    if(!report) return res.status(404).send("Report Not Found")
+    report.title=title
+    report.description=description
+    report.details=details
+    report.status="Resolved"
+    const newReport=await report.save()
+    res.send(newReport)
+}
+module.exports={
+    addReportByClient,
+    addReportByfreelancer,
+    viewReports,
+    viewSpecificReport,
+    resolveReport,
+    deleteReport,
+    updateReport
+}
---
+++ ReportManagement/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const DB_URI = process.env.DB_URI;
+const port = process.env.port || 3090;
+
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB"));
+app.listen(port, () => {
+  console.log(`The Server is running on Port ${port}`);
+});
+
+app.get('/', (req,res) => {
+  res.json({'message':'Hello There!'})
+})
+
