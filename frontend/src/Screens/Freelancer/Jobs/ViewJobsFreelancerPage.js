c74a3b029a55cd7851a0ffa2c10cac9635d9af8f
Yashl-Ammar <yashlammarr@gmail.com>
2024-03-29 08:30:23 +0500

    bug fix2

--- frontend/src/Screens/Freelancer/Jobs/ViewJobsFreelancerPage.js
+++ frontend/src/Screens/Freelancer/Jobs/ViewJobsFreelancerPage.js
@@ -59,11 +59,11 @@ function ViewJobsFreelancerPage() {
     <div className="w-full lg:w-4/5">
         <NavBarFreelancer />
         <div className="text-center">
-            <h1 className="font-heading text-5xl mb-12">View your jobs</h1>
+            <h1 className="font-heading text-5xl mb-12">View jobs</h1>
         </div>
         <div className="bg-aamdanSuperDeepWhite dark:bg-aamdanSuperDeepBlack rounded-xl w-full sm:px-12 py-9">
             <div className="px-12 sm:px-0">
-                <h1 className="font-bold text-5xl mb-10">Jobs you posted</h1>
+                <h1 className="font-bold text-5xl mb-10">Recommended Jobs</h1>
                 <div className="hidden sm:flex mb-12">
                     <div className="w-full lg:w-1/5 text-center">
                         <p className="text-lg font-bold mb-3 px-5" onClick={() => {setSelectedFilter('all')}}><Link>All ({data.length})</Link></p>
@@ -76,8 +76,6 @@ function ViewJobsFreelancerPage() {
                 </div>
                 <div className=" sm:hidden">
                     <select className="mb-5 rounded-lg bg-white dark:bg-aamdanBackground py-3 px-5 border border-strokeColor border-opacity-50 w-full" onChange={(e) => {setSelectedFilter(e.target.value)}}>
-                        <option value='active'>Active Jobs</option>
-                        <option value='removed'>Removed Jobs</option>
                         <option value='all'>All Jobs</option>
                     </select>
                 </div>
